a6d34df1f2d8ba6695633adbc142776b67f96c05
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2019-03-02 23:05:08 +0100

    Update 1.1
    Added left column;
    Project uploaded to Git.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './variables.scss';
+import './index.scss';
+import './intro/intro.scss';
+import './profile/profile.scss';
+import './contacts/contacts.scss';
+import './skills/skills.scss';
