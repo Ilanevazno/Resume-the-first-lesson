7d4dc537b17f0a07b7a0841c8e862832eef6487f
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2019-03-03 19:39:36 +0100

    Working at right column

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import './intro/intro.scss';
 import './profile/profile.scss';
 import './contacts/contacts.scss';
 import './skills/skills.scss';
+import './education/education.scss';
+import './experience/experience.scss';
