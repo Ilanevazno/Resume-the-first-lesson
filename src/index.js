e7eb57cdc9b0345998971a0fb8453defeb086933
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2019-03-10 03:09:00 +0100

    Update 0.1
    Worked at blocks with pixel perfect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import './variables.scss';
+import './tools/variables.scss';
+import './tools/mixins.scss';
 import './index.scss';
 import './intro/intro.scss';
 import './profile/profile.scss';
-import './contacts/contacts.scss';
+import './contacts/contacts.scss'; 
 import './skills/skills.scss';
 import './education/education.scss';
 import './experience/experience.scss';
+import './software/software.scss'
