6e0cd6e67172fd8ff9c4255a4692d0633d5d644a
Ilanevazno <46503497+Ilanevazno@users.noreply.github.com>
2020-03-12 10:04:18 +0300

    https://github.com/Ilanevazno/Resume-the-first-lesson/issues/9 added auto-imports

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-// base
-import './assets/fonts/fonts.scss';
-import './assets/favicons/favicons.js'
+function importAll(resolve) {
+  resolve.keys().forEach(resolve);
+}
 
-//pages
-import './components/contacts/contacts';
-import './components/education/education';
-import './components/experience/experience';
-import './components/intro/intro';
-import './components/profile/profile';
-import './components/skills/skills';
-import './components/software/software';
-
-// //main
-import './styles/index.scss';
-
-//media
-import './styles/media.scss';
+importAll(require.context('./', true, /\.js$|\.scss$/));
